7806aa42e503097a6494aadddbec3b04d509578a
Manny manning <60320088+Abdurahmanm2020@users.noreply.github.com>
2020-04-19 10:59:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,37 +25,37 @@ function makeSound(key) {
 	switch (key) {
 
 	case "a":
-		var tom1 = new Audio('sounds/tom-1.mp3');
+		var tom1 = new Audio('tom-1.mp3');
 		tom1.play();
 	break;
 
 	case "s":
-		var tom2 = new Audio('sounds/tom-2.mp3');
+		var tom2 = new Audio('tom-2.mp3');
 		tom2.play();
 	break;
 
 	case "d":
-		var tom3 = new Audio('sounds/tom-3.mp3');
+		var tom3 = new Audio('tom-3.mp3');
 		tom3.play();
 	break;
 
 	case "h":
-		var tom4 = new Audio('sounds/tom-4.mp3');
+		var tom4 = new Audio('tom-4.mp3');
 		tom4.play();
 	break;
 
 	case "l":
-		var snare = new Audio('sounds/snare.mp3');
+		var snare = new Audio('snare.mp3');
 		snare.play();
 	break;
 
 	case "k":
-		var crash = new Audio('sounds/crash.mp3');
+		var crash = new Audio('crash.mp3');
 		crash.play();
 	break;
 
 	case "j":
-		var kick = new Audio('sounds/kick-bass.mp3');
+		var kick = new Audio('kick-bass.mp3');
 		kick.play();
 	break;
 
